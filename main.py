8e1cf2300334102b3764989f296dad9ca9d4ec07
jnnrbyjn <kennethnarito@gmail.com>
2025-07-09 22:58:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ load_dotenv()
 # ====== CONFIG ======
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID"))
-Bonding_sa_Conste = "Test Channel"  # TEMP for testing
+VOICE_CHANNEL_NAME = "Bonding sa Conste"
 TIMEZONE = pytz.timezone("Europe/London")
 
 # ====== TRACKING DATA ======
